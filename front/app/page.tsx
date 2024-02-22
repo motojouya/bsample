7e3c09b047063fbef0918f384dbad3d5c52c706c
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-02-22 00:58:45 +0000

    next ui

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@nextui-org/react";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>test</p>
+      <Button color="primary">click!</Button>
+    </div>
+  );
 }
