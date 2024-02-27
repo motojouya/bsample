c89bb14311a2e05fc3667a0efa00b0ba6ec60119
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-02-27 01:04:48 +0000

    shadcn/ui

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@nextui-org/react";
+import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
     <div>
       <h1>Hello, Next.js!</h1>
       <p>test</p>
-      <Button color="primary">click!</Button>
+      <Button>click!</Button>
+      <Button variant="outline">Button</Button>
     </div>
   );
 }
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,14 +1,45 @@
 import type { Config } from 'tailwindcss'
 import { nextui } from '@nextui-org/react';
+import { fontFamily } fron 'tailwindcss/defaultTheme';
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
-  plugins: [nextui()],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 }
 
 export default config
