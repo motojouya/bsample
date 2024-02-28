72975190e21144e82430dab24817221da8928cd3
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-02-28 01:05:59 +0000

    a

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,14 +1,13 @@
 import type { Config } from 'tailwindcss'
-import { nextui } from '@nextui-org/react';
 import { fontFamily } fron 'tailwindcss/defaultTheme';
 
 const config: Config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   prefix: "",
   theme: {
