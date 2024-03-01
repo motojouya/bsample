294db8024a583ca87f9f17c0ba22ed623f6d1622
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-03-01 15:01:41 +0000

    top works

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
+import Link from "next/link"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,63 +18,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         {children}
       </body>
     </html>
   );
 }
 
-import { Button } from "@/components/ui/button";
-import Link from "next/link"
-import { buttonVariants } from "@/components/ui/button"
-
-const DashboardHeader = ({ children }) => {
-  return (
-    <div className="flex items-center justify-between px-2">
-      <div className="grid gap-1">
-        <h1 className="font-heading text-3xl md:text-4xl">bsample</h1>
-        <p className="text-lg text-muted-foreground">bsample</p>
-      </div>
-      {children}
-    </div>
-  )
-}
-
 export const Header = () => {
   return (
-    <div className="fixed flex justify-between px-8 w-screen h-16 bg-teal-400 items-center drop-shadow-2xl border-b border-gray-300 shadow-md">
+    <header className="sticky flex justify-between px-8 w-screen h-16 bg-teal-400 items-center drop-shadow-2xl border-b border-gray-300 shadow-md">
       <h1 className="font-bold text-2xl">bsample</h1>
       <div className="flex gap-3">
-        <Button variant="outline">
-          <a href="https://ui.shadcn.com/docs">ログイン</a>
-        </Button>
-        <Button>ログイン</Button>
-      </div>
-    </div>
-  );
-};
-
-const SiteHeader = () => {
-  return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <Link href={'TODO'} target="_blank" rel="noreferrer" >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                <span className="sr-only">ログイン</span>
-              </div>
-            </Link>
-          </nav>
-        </div>
+        <Link href={'/login'} target="_blank" rel="noreferrer" >
+          <div class="w-100 h-20 flex items-center">
+            <span>ログイン</span>
+          </div>
+        </Link>
       </div>
     </header>
-  )
-}
-
+  );
+};
 
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Button>Click me</Button>
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input type="text" placeholder="Any Words" />
+        <Button type="submit">Search</Button>
       </div>
     </main>
   );
