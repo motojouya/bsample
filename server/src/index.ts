c313f51445d7ac37862494402b75366942866772
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-03-25 01:04:20 +0000

    express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,205 @@
-import Fastify from "fastify"
-import { ApolloServer, BaseContext } from "@apollo/server"
-import fastifyApollo, { fastifyApolloDrainPlugin, fastifyApolloHandler } from '@as-integrations/fastify'
 import { readFileSync } from 'fs';
+import http from 'http';
+
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer } from "@apollo/server"
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
+import passport from "passport";
+import LocalStrategy from "passport-local";
 
 import { resolvers } from 'resolver';
 
-const typeDefs = readFileSync('../api/schema/schema.graphql', { encoding: 'utf-8' });
+interface AppContext {
+  token?: string;
+  conn?: object, // TODO 型違う
+  session?: object,
+}
 
 export const run = () => {
-  const server = new ApolloServer({
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const typeDefs = readFileSync('../api/schema/schema.graphql', { encoding: 'utf-8' });
+
+  const apollo = new ApolloServer<AppContext>({
     typeDefs,
     resolvers,
-  });
-
-  const app = Fastify({
-    logger: true,
-  });
-
-  const apollo = new ApolloServer<BaseContext>({
-    typeDefs,
-    resolvers,
-    plugins: [fastifyApolloDrainPlugin(app)],
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
   await apollo.start();
-  await app.register(fastifyApollo(apollo));
 
-  try {
-    await app.listen({ port: 65535 });
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({
+        token: req.headers.token
+        conn: req.context.conn,
+        session: req.session,
+      }),
+    }),
+  );
 
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
   console.log(`🚀  Server ready`);
 };
+
+
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+
+const session = require("express-session");
+const initializePassport = require("./middleware/authMiddleware.js");
+
+const users = [];
+
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", checkAuthenticated, (req, res) => {
+  res.render("index.ejs", { name: req.user.name });
+});
+
+app.get("/login", checkNotAuthenticated, (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post(
+  "/login",
+  checkNotAuthenticated,
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
+
+app.get("/register", checkNotAuthenticated, (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch {
+    res.redirect("/register");
+  }
+});
+
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
+});
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
+
+app.listen(3000);
+
+
+
+
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cookieSession = require("cookie-session");
+const secret = "secretCuisine123";
+
+const app = express();
+
+passport.use(new LocalStrategy({
+    usernameField: "username",
+    passwordField: "password",
+  },function(username, password, done) {
+  }
+));
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [secret],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// authorization
+require("./config/passport")(app);
+
+// router
+app.use('/', require('./routes'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+
